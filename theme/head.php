0cf9d52b420d92d4dd0511c2da652734da96336b
karndevza <karnkung30@gmail.com>
2020-10-29 12:13:38 +0700

    update head seo
    update head seo

--- theme/head.php
+++ theme/head.php
@@ -3,11 +3,25 @@
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-    รายงานทองคำวันนี้
-  </title>
+  <title> รายงานทองคำวันนี้ ประจำวันที่ <?=date("d/m/Y")?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
   <meta http-equiv="expires" content="0">
+  <!--     SEO     -->
+  <meta name="description" content="รายงานราคาทองตามประกาศของสมาคมค้าทองคำ ราคาทองวันนี้ทุกชนิดความบริสุทธิ์ ทั้งทองคำแท่ง ทองรูปพรรณ ราคาทองย้อนหลัง กราฟราคาทอง ข่าวแนวโน้มราคาทองคํา">
+<meta name="keywords" content="ราคาทอง, ราคาทองวันนี้, ราคาทองคําวันนี้, ราคาทองคำวันนี้, ราคาทองย้อนหลัง, ราคาทองคําวันนี้ ฮั่วเซ่งเฮง, ราคาทองรูปพรรณวันนี้, ราคาทองคำแท่งวันนี้, กราฟราคาทอง, ราคาทอง1สลึง, แนวโน้มราคาทองคํา, ทองครึ่งสลึง, ทอง2สลึง, ทอง50สตางค์, ราคาทอง1บาท, ราคาทองคําวันนี้ เยาวราช, ราคาแหวนทอง, ราคาสร้อยทอง, ราคาทอง1กรัม">
+<link rel="next" href="https://vuegold.herokuapp.com/">
+<link rel="canonical" href="https://vuegold.herokuapp.com/">
+<meta property="og:title" content="ราคาทองคําวันนี้"><meta property="og:type" content="website">
+<meta property="og:url" content="https://vuegold.herokuapp.com/">
+<meta property="og:image" content="https://1.bp.blogspot.com/-RwAaTLkIImE/X5ArglLiK9I/AAAAAAAAtyU/o22xT7R6h5k1y-KV43B-GfR0rNcIiZRUwCLcBGAsYHQ/w640-h312/app-banner.png">
+<meta property="og:site_name" content="ราคาทองคําวันนี้"><meta property="og:description" content="ราคาทองวันนี้ตามประกาศสมาคมค้าทองคำ ทองแท่ง ทองรูปพรรณ ราคาทองย้อนหลัง กราฟราคาทอง ข่าวแนวโน้มราคาทอง">
+<meta name="twitter:card" content="summary"><meta name="twitter:title" content="ราคาทองคําวันนี้"><meta name="twitter:description" content="ราคาทองวันนี้ตามประกาศสมาคมค้าทองคำ ทองแท่ง ทองรูปพรรณ ราคาทองย้อนหลัง กราฟราคาทอง ข่าวแนวโน้มราคาทอง">
+<meta name="twitter:image" content="https://1.bp.blogspot.com/-RwAaTLkIImE/X5ArglLiK9I/AAAAAAAAtyU/o22xT7R6h5k1y-KV43B-GfR0rNcIiZRUwCLcBGAsYHQ/w640-h312/app-banner.png">
+<meta itemprop="image" content="https://1.bp.blogspot.com/-RwAaTLkIImE/X5ArglLiK9I/AAAAAAAAtyU/o22xT7R6h5k1y-KV43B-GfR0rNcIiZRUwCLcBGAsYHQ/w640-h312/app-banner.png">
+
+
+
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"> 
